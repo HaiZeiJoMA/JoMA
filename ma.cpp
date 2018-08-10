28dc0a233a677bacecfca51f86237da5993d50c1
joma <860007544@qq.com>
2018-08-10 15:58:07 +0800

    add ma.cpp

---
+++ ma.cpp
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: ma.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 五  8/10 15:47:56 2018
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main() {
+    printf("I love China!\n");
+    return 0;
+}
